58076ff5172b4d82443660bd974b4f60dbdac460
AlanPaccor <136927248+AlanPaccor@users.noreply.github.com>
2023-07-10 12:46:51 -0400

    Add files via upload

--- assets/java.js
+++ assets/java.js
@@ -1,65 +1,62 @@
-var characterLength = 8;
-var choiceArr = [];
+var passwordLength = 8;
+var characterSet = [];
 
-var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '<', '.', '>'];
-var lowerCassArr = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-var upperCassArr = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '<', '.', '>'];
+var lowercaseLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+var uppercaseLetters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-//Assignment Code
-var generateBtn = document.querySelector("#generate")
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-//Add even listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Write password to the #password input
+// Write password to the #password input
 function writePassword() {
-    var correctPrompts = getPrompts(); //returns true or false
-    var passwordText = document.querySelector("#password")
+    var isInputValid = getPrompts(); // Returns true or false
+    var passwordText = document.querySelector("#password");
 
-    if (correctPrompts) {
-    var newPassword = generatePassword();
-    passwordText.value = newPassword;
+    if (isInputValid) {
+        var newPassword = generatePassword();
+        passwordText.value = newPassword;
     } else {
         passwordText.value = "";
     }
 }
 
 function generatePassword() {
-    //I would generatePassword based on the prompts
     var password = "";
-    for(var i = 0; i < characterLength; i++) {
-        var randomIndex = Math.floor(Math.random() * choiceArr.length)
-        password = password + choiceArr[randomIndex];
+    for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randomIndex];
     }
     return password;
 }
 
 function getPrompts() {
-    choiceArr = [];
-    characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters")); //NaN
+    characterSet = [];
+    passwordLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters)"));
 
-    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) { //I'm hoping that this is all false
-    alert("Character length has to be a number, 8 - 128 digits. Please try again.");
-    return false;
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        alert("Password length must be a number between 8 and 128. Please try again.");
+        return false;
     }
 
-    if (confirm("Would you like lowercase letters in your password?")) {
-        choiceArr = choiceArr.concat(lowerCassArr);
+    if (confirm("Would you like to include lowercase letters in your password?")) {
+        characterSet = characterSet.concat(lowercaseLetters);
     }
-    if (confirm("Would you like uppercase letters in your password?")) {
-        choiceArr = choiceArr.concat(upperCassArr);
+    if (confirm("Would you like to include uppercase letters in your password?")) {
+        characterSet = characterSet.concat(uppercaseLetters);
     }
-    if (confirm("Would you like special characters in your password?")) {
-        choiceArr = choiceArr.concat(specialCharArr);
+    if (confirm("Would you like to include special characters in your password?")) {
+        characterSet = characterSet.concat(specialCharacters);
     }
-    if (confirm("Would you like numbers in your password?")) {
-        choiceArr = choiceArr.concat(numberArr);
+    if (confirm("Would you like to include numbers in your password?")) {
+        characterSet = characterSet.concat(numbers);
     }
     return true;
 }
-
-
 
 const copyButton = document.getElementById("copy");
 const passwordTextarea = document.getElementById("Password");
